8c4a28d9f205ba96858644bdca37d07a26524ade
a.sharifi <a.sharifi@ncdrc.info>
2022-12-29 23:21:54 +0330

    github pages for docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="GeneticAlgorithmFeatureSelection",
-    version="0.0.1",
+    version="0.0.2",
     author="Ali Sharifi",
     author_email="alisharifisearch@gmail.com",
     description="Feature Selection with Genetic Algorithm",
