317763062571607eb670d81e7707d002cf2afd03
Ali Sharifi <alisharifisearch@gmail.com>
2023-01-22 13:51:46 +0330

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import make_classification
 import pandas as pd
-from src.genetic_algoirthm.GA import GenticAlgorithmFeatureSelection
+
+from GeneticAlgorithmFeaturesSelection.GA import FeatureSelection
 
 # build sample dataset
 x, y = make_classification(n_features=65, n_samples=1000)
@@ -11,11 +12,11 @@ features = pd.DataFrame(x, columns=columns)
 target = pd.DataFrame(y, columns=['target'])
 
 # run feature selection
-GA = GenticAlgorithmFeatureSelection(features=features, target=target, population_size=100, elite_rate=0.5,
-                                     fitness_alpha=0.55, tourn_size=25, no_generation=50)
-GA.run()
+FS = FeatureSelection(features=features, target=target, population_size=100, elite_rate=0.5,
+                      fitness_alpha=0.55, tourn_size=25, no_generation=50)
+FS.run()
 
-history = GA.history
+history = FS.history
 
 for generation, detail in history.items():
     print(f'Generation :{generation}')
