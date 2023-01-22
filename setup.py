38f53e6856d3ece7f320294bf02c7f7f1cb6d404
Ali Sharifi <alisharifisearch@gmail.com>
2023-01-22 13:45:38 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+pth = os.path.dirname(os.path.realpath(__file__))
+requirementPath = pth + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
 setuptools.setup(
-    name="GeneticAlgorithmFeatureSelection",
-    version="0.0.2",
+    name="GeneticAlgorithmFeaturesSelection",
+    version="0.0.3",
     author="Ali Sharifi",
     author_email="alisharifisearch@gmail.com",
     description="Feature Selection with Genetic Algorithm",
@@ -20,7 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.9"
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+    install_requires=install_requires
 )
